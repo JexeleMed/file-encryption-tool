4c6b772950f35e44adf2e14f9aa5e17204b5131b
jexelem <jexelem@gmail.com>
2025-01-02 01:42:39 +0100

    Deleted euclide feature | Keygen feature added | Bytes to HEX translation added

--- main.cpp
+++ main.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
-#include <cmath>
-#include <regex>
+#include <random>
+#include <array>
+#include <iomanip>
+#include <sstream>
 
-// FINDING GCD
-long long int euclide(long long int a, long long int b) {
-    if(a<b)
-        std::swap(a, b);
-    if(a%b == 0)
-        return b;
+// Key generator
+std::array<uint8_t, 16> generateKey() {
+    std::array<uint8_t, 16> key;
+    std::random_device rd;
 
-    return euclide(b,a%b);
+    for(auto& byte : key) {
+        byte = rd() % 256;
+    }
+
+    return key;
 }
 
+
+// BYTES to HEX
+template <std::size_t N>
+std::string toHex(const std::array<uint8_t, N>& key) {
+    std::ostringstream oss;
+    for (uint8_t byte : key) {
+        oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
+    }
+    return oss.str();
+}
+
+
+
 int main() {
-
-    long int p = 9223372036854775807;
-    long int q = 9223372036854775783;
-
-    // int n = p * q;
-    std::cout << euclide(p,q);
-    // int phi = (p - 1) * (q - 1);
+    auto key = generateKey();
+    auto iv = generateKey();
+    std::string hexKey = toHex(key);
+    std::string hexIV = toHex(iv);
+    std::cout << "Hex Key: " << hexKey << std::endl;
     return 0;
 
 }
