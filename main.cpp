91c63f4abedb4081737830e4a0a1db80769b1352
jexelem <jexelem@gmail.com>
2025-01-03 19:36:19 +0100

    Add file loader to read 16-byte chunks in binary mode with hex output

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 #include <array>
 #include <iomanip>
 #include <sstream>
+#include <fstream>
+#include <vector>
 
 // Key generator
 std::array<uint8_t, 16> generateKey() {
-    std::array<uint8_t, 16> key;
+    std::array<uint8_t, 16> key = {};
     std::random_device rd;
 
     for(auto& byte : key) {
@@ -27,6 +29,43 @@ std::string toHex(const std::array<uint8_t, N>& key) {
     return oss.str();
 }
 
+// Load file to buffer and apply padding
+void load(const std::string& name) {
+    std::ifstream file(name, std::ios::binary);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open the file: " << name << std::endl;
+        return;
+    }
+
+    constexpr  size_t chunkSize = 16;
+    std::vector<char> buffer(chunkSize);
+    size_t chunkIndex = 0;
+    while (file.read(buffer.data(), chunkSize) || file.gcount() > 0) {
+        size_t bytesRead = file.gcount();
+        std::cout << "Chunk " << chunkIndex++ << " bytes read: " << bytesRead << std::endl;
+        for(size_t i = 0; i < bytesRead; ++i) {
+            std::cout << std::hex << std::setw(2) << std::setfill('0') << (0xff & buffer[i]) << " ";
+        }
+        std::cout << std::endl;
+
+        if (bytesRead < chunkSize) {
+            size_t paddingLength = chunkSize - bytesRead;
+            std::cout << "Padding applied: " << paddingLength << " bytes" << std::endl;
+
+            for(size_t i = bytesRead; i < chunkSize; ++i) {
+                buffer[i] = static_cast<char>(paddingLength);
+            }
+            std::cout << "Padded chunk: ";
+            for(size_t i = 0; i < chunkSize; ++i) {
+                std::cout <<  std::hex << std::setw(2) << std::setfill('0') << (0xff & buffer[i]) << " ";
+            }
+            std::cout << std::endl;
+        }
+
+    }
+
+    file.close();
+}
 
 
 int main() {
@@ -35,6 +74,10 @@ int main() {
     std::string hexKey = toHex(key);
     std::string hexIV = toHex(iv);
     std::cout << "Hex Key: " << hexKey << std::endl;
+    std::cout << "Hex IV: " << hexIV << std::endl;
+
+    std::string path = "sample.txt";
+    load(path);
     return 0;
 
 }
